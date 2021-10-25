3f34a94a915f1614df36260110e9b8b95a593816
guyskv <guyskv@gmail.com>
2021-10-25 19:07:33 +0300

    Game Done

--- app/src/main/java/com/guy/chickeninvaders/activities/Activity_Panel.java
+++ app/src/main/java/com/guy/chickeninvaders/activities/Activity_Panel.java
@@ -16,10 +16,10 @@ import java.util.ArrayList;
 
 public class Activity_Panel extends AppCompatActivity {
 
+    private final int MAX_LIVES = 4;
+
     private ImageView panel_IMG_food;
-    private ImageView panel_IMG_heart1;
-    private ImageView panel_IMG_heart2;
-    private ImageView panel_IMG_heart3;
+    private ImageView[] panel_IMG_hearts;
     private TextView panel_LBL_score;
     private Button panel_BTN_yes;
     private Button panel_BTN_no;
@@ -27,12 +27,13 @@ public class Activity_Panel extends AppCompatActivity {
     private ArrayList<Food> foods;
     private int index = 0;
     private int score = 0;
-    private int lives = 3;
+    private int lives = MAX_LIVES;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_panel);
+
 
         findViews();
 
@@ -90,25 +91,27 @@ public class Activity_Panel extends AppCompatActivity {
     }
 
     private void updateLivesViews() {
-        if (lives >= 3) {
-            return;
-        } else if (lives == 2) {
-            panel_IMG_heart3.setVisibility(View.INVISIBLE);
-        } else if (lives == 1) {
-            panel_IMG_heart2.setVisibility(View.INVISIBLE);
-        } else if (lives == 0) {
-            panel_IMG_heart1.setVisibility(View.INVISIBLE);
+        for (int i = panel_IMG_hearts.length-1; i >= 0; i--) {
+            if ((i+1) >= lives) {
+                panel_IMG_hearts[i].setVisibility(View.VISIBLE);
+            } else {
+                panel_IMG_hearts[i].setVisibility(View.INVISIBLE);
+            }
         }
     }
 
 
     private void findViews() {
         panel_IMG_food = findViewById(R.id.panel_IMG_food);
-        panel_IMG_heart1 = findViewById(R.id.panel_IMG_heart1);
-        panel_IMG_heart2 = findViewById(R.id.panel_IMG_heart2);
-        panel_IMG_heart3 = findViewById(R.id.panel_IMG_heart3);
         panel_LBL_score = findViewById(R.id.panel_LBL_score);
         panel_BTN_yes = findViewById(R.id.panel_BTN_yes);
         panel_BTN_no = findViewById(R.id.panel_BTN_no);
+
+        panel_IMG_hearts = new ImageView[] {
+                findViewById(R.id.panel_IMG_heart1),
+                findViewById(R.id.panel_IMG_heart2),
+                findViewById(R.id.panel_IMG_heart3),
+                findViewById(R.id.panel_IMG_heart4)
+        };
     }
 }
